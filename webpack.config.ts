6de35de7f08af458d5e80c427ab29836a67225e1
Marc Wilson <mwilson@marcswilson.com>
2017-09-06 18:21:43 -0700

    removed babel; added ignore plugin for vertx (webpack)

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,13 @@
+import * as webpack from 'webpack';
 
 module.exports = {
     plugins: [
+        new webpack.IgnorePlugin(/vertx/)
     ],
     entry: './src/server.js',
     output: {
         path: __dirname + '/dist',
-        filename: 'servers.js'
+        filename: 'server.js'
     },
     target: 'node'
 }
