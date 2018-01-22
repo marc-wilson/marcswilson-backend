c8ef88b7a73b3862a9d7e91b5c218491804ebfc5
Marcus Wilson <mswilson4040@hotmail.com>
2018-01-21 18:59:27 -0800

    socket io for mlb db update

--- src/modules/mlbstatsdb/mlb-stats-db.ts
+++ src/modules/mlbstatsdb/mlb-stats-db.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs-extra';
+import * as download from 'download-git-repo';
 
 export class MlbStatsDb {
     public socket: any = null;
@@ -7,15 +8,17 @@ export class MlbStatsDb {
         this.socket.on('updateDatabase', this.init.bind(this));
     }
     init(): void {
-        let num = 0;
-        const interval = setInterval( () => {
-            if (num < 100) {
-                num += 1;
-                this.socket.emit('progress', { progress: num });
-            } else {
-                clearInterval(interval);
-            }
-        }, 500);
+        this.socket.emit('progress', { progress: `Step 1/12: Removing BaseballDatabank local repo` });
+        this.removeDatabankRepo().then( _result => {
+            this.socket.emit('progress', { progress: `Step 1/12: Finished removing BaseballDatabank local repo`})
+        }, error => {
+            this.socket.emit('progress', { progress: `Error removing databank repo`});
+        }).then( () => {
+            this.socket.emit('progress', { progress: `Step 2/12: Cloning baseball databank repo`})
+            this.cloneRepository().then( _result => {
+                this.socket.emit('progress', { progress: `Step 2/12: Finished cloning baseball databank`})
+            });
+        })
     }
     removeDatabankRepo(): Promise<any> {
         return new Promise( (resolve, reject) => {
@@ -26,6 +29,23 @@ export class MlbStatsDb {
                     resolve(true);
                 }
             })
+        });
+    }
+    cloneRepository(): Promise<any> {
+        return new Promise( (resolve, reject) => {
+            this.removeDatabankRepo().then( res => {
+                if (res) {
+                    download('chadwickbureau/baseballdatabank', 'baseballdatabank', (err) => {
+                        if (!err) {
+                            resolve(true);
+                        } else {
+                            reject(err);
+                        }
+                    })
+                } else {
+                    reject(null);
+                }
+            });
         });
     }
 }
