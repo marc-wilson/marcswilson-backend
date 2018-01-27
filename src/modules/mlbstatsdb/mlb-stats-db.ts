2ff3b53ca4b9aae1e6f44fb3cccade1a109ccc44
Marcus Wilson <mswilson4040@hotmail.com>
2018-01-26 21:17:45 -0800

    removed await

--- src/modules/mlbstatsdb/mlb-stats-db.ts
+++ src/modules/mlbstatsdb/mlb-stats-db.ts
@@ -118,7 +118,7 @@ export class MlbStatsDb {
                 const promises = files.map( async f => {
                     const name = this.getCollectionNameFromFile(f);
                     if (name) {
-                        return await this.createCollection( name, f );
+                        return this.createCollection( name, f );
                     }
                 });
                 Promise.all(promises).then( results => {
