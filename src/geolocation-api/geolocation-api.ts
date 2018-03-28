ee44202e91234eed6e501d0c4f7d35fcb04c5a1b
Marcus Wilson <mswilson4040@hotmail.com>
2018-03-27 19:47:39 -0700

    installed request promises native; moved client call to get topojson to server

--- src/geolocation-api/geolocation-api.ts
+++ src/geolocation-api/geolocation-api.ts
@@ -8,14 +8,23 @@ export class GeoLocationApi {
         this.express = require('express');
         this.request = require('request');
         this.cities = require('cities');
+        this.request = require('request-promise-native');
         this.router = this.express.Router();
 
         this.router.get('/city/:city/state/:state', (request, response) => {
             const ret = this.cities.findByCityAndState(request.params.city, request.params.state);
             response.json(ret);
         });
+        this.router.get('/topojson/us', async (request, response) => {
+            const topo = await this.getUSTopoJson();
+            response.status(200).json(topo);
+        })
 
         module.exports = this.router;
+    }
+    async getUSTopoJson(): Promise<any> {
+        const topojson = await this.request.get('https://bl.ocks.org/mbostock/raw/4090846/us.json');
+        return JSON.parse(topojson);
     }
 
 }
